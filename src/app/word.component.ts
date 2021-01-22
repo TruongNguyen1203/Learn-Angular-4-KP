896f1c4742dbff972602ccdd5d9ba7f8ba569ef0
Trương Nguyên <truongnguyen@Truong-Nguyen.local>
2021-01-22 15:45:36 +0700

    Tao component don gian

---
+++ src/app/word.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core'; //import component tu angular core
+
+
+//cung cap thong tin quan trong cho angular biet component nay la gi
+@Component({
+  template : '<h3>This is Word Component</h3>',
+  selector: 'app-word'
+})
+
+export class WordComponent{}
