aff2bd1acbe7919e5dc1b9d5fc25df1e27850929
Trương Nguyên <truongnguyen@Truong-Nguyen.local>
2021-01-22 16:01:47 +0700

    Bai 5 : Component decorators

--- src/app/word.component.ts
+++ src/app/word.component.ts
@@ -3,8 +3,9 @@ import {Component} from '@angular/core'; //import component tu angular core
 
 //cung cap thong tin quan trong cho angular biet component nay la gi
 @Component({
-  template : '<h3>This is Word Component</h3>',
-  selector: 'app-word'
+  templateUrl : './word.component.html',
+  selector: 'app-word',
+  styleUrls: ['./word.component.css']
 })
 
 export class WordComponent{}
