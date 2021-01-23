d7dab31b6bb8a0fa7dcfaa31a9b7bccea2f31982
Trương Nguyên <truongnguyen@Truong-Nguyen.local>
2021-01-23 12:05:02 +0700

    Bai 11 : Xu ly Text input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {WordComponent} from './word/word.component';
-import { BookComponent } from './book/book.component'
+import { BookComponent } from './book/book.component';
+import { UserFormComponent } from './user-form/user-form.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     WordComponent,
-    BookComponent
+    BookComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
