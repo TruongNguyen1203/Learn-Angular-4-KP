44d4d2820100f8722b99abe67e10d972db1a2717
Trương Nguyên <truongnguyen@Truong-Nguyen.local>
2021-02-08 20:42:55 +0700

    Bai 16: ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { BookComponent } from './book/book.component';
 import { UserFormComponent } from './user-form/user-form.component'
 
 import {FormsModule} from '@angular/forms';
+import { StructComponent } from './struct/struct.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WordComponent,
     BookComponent,
-    UserFormComponent
+    UserFormComponent,
+    StructComponent
   ],
   imports: [
     BrowserModule,
